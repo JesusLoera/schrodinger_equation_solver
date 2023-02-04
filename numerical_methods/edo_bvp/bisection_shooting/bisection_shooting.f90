91dff17ae9bfce9f4a6bd76ad19954494363086b
JesusLoera <jesusloera45@gmail.com>
2023-02-04 16:41:56 -0600

    Se agrega método de bisección en Fortran

---
+++ numerical_methods/edo_bvp/bisection_shooting/bisection_shooting.f90
@@ -0,0 +1,157 @@
+! Programa elaborado por Jesús Eduardo Loera Casas
+! Fecha 04/02/23
+
+! En este programa se implementa el método de shooting para resolver
+! edo sujetas a bvp del tipo
+
+!     y''(x) = f(y'(x),y(x),x) ; y(xo)=a , y(xn)=b
+
+! Convertimos la edo bvp en una edo ivp y tratamos de adivinar
+! la condición inicial que satisfaga las condiciones en la frontera
+
+!     u_E''(x) = f(u_E'(x),u_E(x),x) ; u_E(xo)=a , u'_E(xo)=E
+
+! Consideramos la diferencia entre dos soluciones en la frontera
+
+!     g(E) = u_E(xn) - y(xn) = u_E(xn) - b 
+
+! Se adivina E y se resuelve la ivp hasta satisfacer g(E) = 0
+
+        ! Definimos la edo a resolver
+        real function f(z, y, x)
+            implicit none
+            real :: x,y,z
+            f = -9.8
+            return
+        end  
+
+        ! Definimos la función de convergencia
+        real function g(Un, Fn)
+            implicit none 
+            real Un, Fn
+            g = Un - Fn
+            return
+        end
+
+        ! Definimos el programa principal    
+        program main
+
+            implicit none
+            integer :: i, N, step
+            real :: xmin, xmax, dx, xo, yo, yn
+            real :: emax, emin, root
+            real, allocatable :: X(:), Y(:), Z(:)
+
+            ! Definimos la malla de integración
+            dx = 0.001 ; xmin = 0.0 ; xmax = 5.0
+            N = (xmax - xmin)/dx + 1
+
+            ! Definimos el intervalo de busqueda para adivinar
+            ! la condición inicial que define el bvp
+            emin = 0.0 ; emax = 50.0
+
+            ! Condicion frontera
+            xo = xmin  ;  yo = 0   ;  yn = 50
+
+            ! Definimos el tamaño del arreglo
+            allocate(X(1:N), Y(1:N), Z(1:N))
+            do i = 0, N-1
+                X(i+1) = xmin + i*dx
+                Y(i+1) = 0
+                Z(i+1) = 0
+            end do
+            Y(1) = yo
+            
+            ! Llamamos al método de bisection shooting
+            call bisect_shooting(X,Y,Z,N,dx,emin,emax,yn,root,step)
+            write(*,*) "Se encontro una raiz en ", step," iteraciones"
+            write(*,*) "La raiz encontrada es x = ", root
+
+            ! write array to a file
+            open(10, file='bisect_shooting.dat')
+            do i=1,N
+                write(10,*) X(i), Y(i), Z(i)
+            end do
+            close(10)
+
+            ! Despejamos los arreglos de la memoria
+            deallocate(X,Y,Z)
+
+        end program main
+
+        ! Método de Runge-Kutta para resolver edo ivp
+
+        subroutine rk4o2(X, Y, Z, N, dx)
+            implicit none
+            integer :: N, i
+            real, dimension(N) :: X, Y, Z
+            real :: dx, f, k1, k2, k3, k4, i1, i2, i3, i4
+            do i = 1, N-1
+                k1 = Z(i)
+                i1 = f(Z(i), Y(i), X(i))
+                k2 = Z(i) + (0.5)*i1*dx
+                i2 = f(Z(i)+0.5*i1*dx, Y(i)+0.5*k1*dx, X(i)+0.5*dx)
+                k3 = Z(i) + (0.5)*i2*dx
+                i3 = f(Z(i)+0.5*i2*dx, Y(i)+0.5*k2*dx, X(i)+0.5*dx)
+                k4 = Z(i) + i3*dx
+                i4 = f(Z(i)+i3*dx, Y(i)+k3*dx, X(i)+dx)
+                Y(i+1) = Y(i) + (1.0/6)*dx*(k1+2*k2+2*k3+k4)
+                Z(i+1) = Z(i) + (1.0/6)*dx*(i1+2*i2+2*i3+i4)
+            end do
+        end subroutine rk4o2
+
+        ! Método de bisección para hallar el valor inicial
+        ! que satisfaga las condiciones frontera exigidas.
+
+        subroutine bisect_shooting(X,Y,Z,N,dx,a,b,Fn,root,step)
+            implicit none
+            real :: a, b, E, root, g, tol, Fn, dx, Un, aux, dE, dy
+            real, dimension(N) :: X, Y, Z
+            integer :: steps, step, i, N
+            ! definimos una tolerancia para la convergencia
+            tol = 0.9 
+            ! definimos un número iteraciones maxima para la convergencia
+            steps = 10000
+
+            do i = 1, steps
+                E = 0.5*(a+b)
+                dE = b-a
+                Z(1) = a 
+                call rk4o2(X, Y, Z, N, dx)
+                aux = Z(N) 
+                Z(1) = E
+                call rk4o2(X, Y, Z, N, dx)
+                Un = Z(N)
+                dy = Un - Fn
+                write(*,*) E
+                if (  abs(dy) .le. tol ) then
+                    step = i
+                    root = E 
+                    exit
+                end if
+
+                if ( g(aux, Fn)*g(Un, Fn)>0 ) then
+                    a = E
+                else if ( g(aux, Fn)*g(Un, Fn)<0 ) then
+                    b = E
+                else 
+                    if ( g(Un, Fn)==0 ) then
+                        write(*,*) "Hay una raiz en x = ", a
+                        step = i
+                        root = a
+                        exit
+                    end if
+                    if ( g(Un, Fn)==0 ) then
+                        write(*,*) "Hay una raiz en x = ", E
+                        step = i
+                        root = E
+                        exit
+                    end if
+                end if
+            end do
+            if ( i == steps ) then
+                write(*,*) "No se encontro raiz que satisfaga la"
+                write(*,*) "tolerancia", tol, " en ", steps, " pasos" 
+            end if
+    
+        end subroutine bisect_shooting
---
+++ numerical_methods/edo_ivp/RK4_O2/rk4_o2.f90
@@ -0,0 +1,78 @@
+! Programa elaborado por Jesús Eduardo Loera Casas
+! Fecha 03/02/23
+
+! En este programa se implementa el método de Runge-Kutta de
+! cuarto orden para resolver ecuaciones diferenciales del tipo
+
+!     y''(x) = f(y'(x),y(x),x) ; y(0)=yo y y'(0)=zo
+
+! Usando los siguientes cambios de variable y'(x)=z(X), y''(x)=z'(x)
+
+!    z'(x) = f(z(x),y(x),x)   y   y'(x)=z(x)  ;   y(0)=yo y z(0)=zo
+
+! Este algoritmo es más preciso que el algoritmo de Euler al basarse en
+! la derivada central en lugar de la derivada hacia delante.
+        
+        real function f(z, y, x)
+            implicit none
+            real :: x,y,z
+            f = 5*(x-z)
+            return
+        end  
+
+        subroutine rk4o2(X, Y, Z, N, dx)
+            implicit none
+            integer :: N, i
+            real, dimension(N) :: X, Y, Z
+            real :: dx, f, k1, k2, k3, k4, i1, i2, i3, i4
+            do i = 1, N-1
+                k1 = Z(i)
+                i1 = f(Z(i), Y(i), X(i))
+                k2 = Z(i) + (0.5)*i1*dx
+                i2 = f(Z(i)+0.5*i1*dx, Y(i)+0.5*k1*dx, X(i)+0.5*dx)
+                k3 = Z(i) + (0.5)*i2*dx
+                i3 = f(Z(i)+0.5*i2*dx, Y(i)+0.5*k2*dx, X(i)+0.5*dx)
+                k4 = Z(i) + i3*dx
+                i4 = f(Z(i)+i3*dx, Y(i)+k3*dx, X(i)+dx)
+                Y(i+1) = Y(i) + (1.0/6)*dx*(k1+2*k2+2*k3+k4)
+                Z(i+1) = Z(i) + (1.0/6)*dx*(i1+2*i2+2*i3+i4)
+            end do
+        end subroutine
+
+        program main
+
+            implicit none
+            integer :: i, N
+            real :: xmin, xmax, dx, xo, yo, zo
+            real, allocatable :: X(:), Y(:), Z(:)
+
+            ! Definimos la malla de integración
+            dx = 0.001 ; xmin = 0.0 ; xmax = 1.0
+            N = (xmax - xmin)/dx + 1
+
+            ! Condicion inicial
+            xo = xmin  ;  yo = 1  ;  zo = 3.59655
+
+            ! Definimos el tamaño del arreglo
+            allocate(X(1:N), Y(1:N), Z(1:N))
+            do i = 0, N-1
+                X(i+1) = xmin + i*dx
+                Y(i+1) = 0
+                Z(i+1) = 0
+            end do
+            Y(1)=yo  ; Z(1)=zo
+
+            ! Llamamos el método de Euler
+            call rk4o2(X, Y, Z, N, dx)
+
+            ! write array to a file
+            open(10, file='rk4o2.dat')
+            do i=1,N
+                write(10,*) X(i), Y(i), Z(i)
+            end do
+            close(10)
+
+            ! Despejamos los arreglos de la memoria
+            deallocate(X,Y,Z)
+
+        end program main
